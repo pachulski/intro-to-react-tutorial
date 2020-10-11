0320b16fb4cf718cf28628ef22cf16608a752f1a
Radek Pachulski <radoslaw.pachulski@gmail.com>
2020-10-11 19:21:33 +0200

    taking turns functionality added

--- src/index.js
+++ src/index.js
@@ -17,13 +17,17 @@ function Square(props) {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
 
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -35,7 +39,7 @@ function Square(props) {
     }
 
     render() {
-      const status = 'Next player: X';
+      const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
       return (
         <div>
